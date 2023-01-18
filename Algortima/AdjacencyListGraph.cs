8bb5334175acf59419bbbf16ea4c6d015b42f283
Mehmet <m_akar02@yahoo.com>
2023-01-19 00:30:14 +0300

    Graph

---
+++ Algortima/AdjacencyListGraph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Algortima
+{
+	public class AdjacencyListGraph
+	{
+		private int numVertices;
+		private Dictionary<int, List<int>> AdjacencyListOfVertices;
+
+		public AdjacencyListGraph(int v)
+		{
+			numVertices = v;
+			AdjacencyListOfVertices = new Dictionary<int, List<int>>();
+		}
+
+		public void AddAdge(int vertice,int adjacencyVertice)
+		{
+			if (!AdjacencyListOfVertices.ContainsKey(vertice))
+				AdjacencyListOfVertices.Add(vertice, new List<int>());
+
+			if (AdjacencyListOfVertices[vertice].Contains(adjacencyVertice))
+				return;
+
+			AdjacencyListOfVertices[vertice].Add(adjacencyVertice);
+        }
+
+    }
+}
+
--- Algortima/Program.cs
+++ Algortima/Program.cs
@@ -3,7 +3,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        AdjacencyMatrixOfGraph g = new AdjacencyMatrixOfGraph(4);
+
+        g.AddEdge(0, 1);
+        g.AddEdge(0, 2);
+        g.AddEdge(1, 2);
+        g.AddEdge(2, 0);
+        g.AddEdge(2, 3);
+
+        Console.WriteLine(g.toString());
     }
 }
 
